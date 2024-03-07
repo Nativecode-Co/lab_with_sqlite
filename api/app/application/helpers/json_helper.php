874d90372323f19039e437071f3f8261d8e2eb84
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2024-03-07 08:16:05 +0300

    update json helper to filter with age and gender

--- api/app/application/helpers/json_helper.php
+++ api/app/application/helpers/json_helper.php
@@ -1,62 +1,4 @@
 <?php
-/*
-{
-  "component": [
-    {
-      "name": "Anti-Proteinase-3",
-      "result": "number",
-      "options": [],
-      "shortcut": "",
-      "reference": [
-        {
-          "kit": "10",
-          "note": "",
-          "unit": "16531501191676627",
-          "range": [
-            {
-              "low": "5",
-              "high": "10",
-              "name": "low"
-            },
-            {
-              "low": "10",
-              "high": "20",
-              "name": "high"
-            }
-          ],
-          "gender": "ذكر",
-          "age low": "20",
-          "age high": "60",
-          "age unit low": "عام",
-          "age unit high": "عام"
-        },
-        {
-          "kit": "10",
-          "note": "",
-          "unit": "16531501191676627",
-          "range": [
-            {
-              "low": "5",
-              "high": "10",
-              "name": "low"
-            },
-            {
-              "low": "10",
-              "high": "20",
-              "name": "high"
-            }
-          ],
-          "gender": "ذكر",
-          "age low": "20",
-          "age high": "60",
-          "age unit low": "عام",
-          "age unit high": "عام"
-        }
-      ]
-    }
-  ]
-}
-*/
 class Json
 {
   public function __construct($string)
@@ -82,15 +24,59 @@ class Json
     $refrences = array_filter($refrences, function ($refrence) use ($fields) {
       $result = true;
       foreach ($fields as $key => $value) {
-        if (isset($refrence[$key])) {
-          if ($refrence[$key] != $value) {
+        if (isset ($refrence[$key]) || $key == 'age' || $key = 'gender') {
+          if ($key == 'age') {
+            $ageRange = $this->getAgeRange($refrence);
+            $age = $value * 365; // convert age to days
+            // if age out of range return false
+            if ($age < $ageRange['low'] || $age > $ageRange['high']) {
+              $result = false;
+            }
+          } else if ($key == 'gender') {
+            if ($refrence[$key] != 'كلاهما' && $refrence[$key] != $value) {
+              $result = false;
+            }
+          } else if ($refrence[$key] != $value) {
             $result = false;
           }
         }
       }
       return $result;
     });
-    return $refrences;
+    return array_values($refrences);
+  }
+
+  public function getAgeRange($refrence)
+  {
+    $ageLow = $refrence['age low'];
+    $ageHigh = $refrence['age high'];
+    $ageUnitLow = $refrence['age unit low'];
+    $ageUnitHigh = $refrence['age unit high'];
+    switch ($ageUnitLow) {
+      case 'عام':
+        $ageLow = $ageLow * 365;
+        break;
+      case 'شهر':
+        $ageLow = $ageLow * 30;
+        break;
+      default:
+        break;
+    }
+
+    switch ($ageUnitHigh) {
+      case 'عام':
+        $ageHigh = $ageHigh * 365;
+        break;
+      case 'شهر':
+        $ageHigh = $ageHigh * 30;
+        break;
+      default:
+        break;
+    }
+    return [
+      'low' => $ageLow,
+      'high' => $ageHigh
+    ];
   }
 
 }
