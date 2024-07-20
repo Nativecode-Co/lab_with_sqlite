70af5f06121ed9028709d1df9555cc8d95bdca3d
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2024-07-20 11:44:40 +0300

    update

--- api/app/application/helpers/db_helper.php
+++ api/app/application/helpers/db_helper.php
@@ -2,8 +2,8 @@
 
 function create_hash()
 {
-    return round(microtime(true) * 10000) . rand(0, 1000);
-    //    return generateUnique9DigitNumber();
+    // return round(microtime(true) * 10000) . rand(0, 1000);
+    return generateUnique9DigitNumber();
 }
 
 function generateUnique9DigitNumber()
